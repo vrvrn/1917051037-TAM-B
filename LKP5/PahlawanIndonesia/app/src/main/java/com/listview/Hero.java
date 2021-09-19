16d876c722fa64324b4ce840e9c8374ca109d45f
vrvrn <viravrna@gmail.com>
2021-09-19 19:46:37 +0700

    Baru

---
+++ LKP5/PahlawanIndonesia/app/src/main/java/com/listview/Hero.java
@@ -0,0 +1,31 @@
+package com.listview;
+
+public class Hero {
+    private int foto;
+    private String nama;
+    private String deskripsi;
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
